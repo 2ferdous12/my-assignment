ebfa4cca0239ac74eecdb214dd7f9e622b25233f
Ferdous020 <sheikhferdous75@gmail.com>
2024-02-27 16:56:22 -0800

    added all file and style

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function busTicket(){
       count = count + 1;
       document.getElementById('bus-count').innerText = count;
   const btnName = (e.target.childNodes[1].innerText);
+  const price = document.getElementById('main-price').innerText;
+ 
+const firstBusCount = getConvertValue('bus-count');
+if(firstBusCount + 1 > 5){
+  alert('limit complete');
+}
+
+  const seat = getConvertValue('total-sit');
+  document.getElementById('total-sit').innerText = seat - 1;
+
 
  const economys = document.getElementById('economy');
 const tr = document.createElement('tr')
@@ -16,7 +26,7 @@ const tr = document.createElement('tr')
  const td2 = document.createElement('td');
  td2.innerText = 'economy';
  const td3 = document.createElement('td');
- td3.innerText = '550';
+ td3.innerText = price;
  tr.appendChild(td);
  tr.appendChild(td2);
  tr.appendChild(td3);
@@ -24,44 +34,66 @@ const tr = document.createElement('tr')
  economys.appendChild(tr);
 
 
-
- const totalCost = document.getElementById('Total-Cost').innerText;
- const convertTotalCost = parseInt(totalCost);
- document.getElementById('Total-Cost').innerText = convertTotalCost + parseInt('550');
-
- const grandTotal = document.getElementById('Grand-total').innerText;
-
- const convertGrandTotal = parseInt(grandTotal);
- document.getElementById('Grand-total').innerText = convertGrandTotal + parseInt('550');
-
-
-
-     
+ 
       addElement(e.target.classList.add('bg-green-500'));
-
-
-
-
+      updateTotalcost(price);
+      updateGranTotalCost();
+  
     })
   }
  
   
     hideElement('first-section');
     hideElement('first-scound-section');
-    removeElement('middle-section');
     
     ;
-    
-    
+
+}
+
+function updateTotalcost(value){
+  const totalcost = getConvertValue("Total-Cost");
+  const sum = totalcost + parseInt(value);
+  document.getElementById("Total-Cost").innerText = sum;
+}
+
+function updateGranTotalCost(status){
+  const totalcost = getConvertValue("Total-Cost");
+  if(status == undefined){
+    document.getElementById('Grand-total').innerText = totalcost;
+  }
+  else{
+    const copunCode = document.getElementById('coupun-code').value;
+    if(copunCode == 'Couple 20'){
+      const discount = totalcost * 0.2;
+      document.getElementById('Grand-total').innerText = totalcost - discount;
+    }
+else if(copunCode == 'NEW15'){
+    const discount = totalcost * 0.15;
+    document.getElementById('Grand-total').innerText = totalcost - discount;
+    }
+    else{
+      alert('please enter vaild coupon Code');
+    }
+  }
+  
+ 
+ 
+
 }
 
 
   function addBtn(){
     removeElement('verify-section');
-    hideElement('middle-section');
     applyButton(catagory);
     
   }
+
+  function getConvertValue(id){
+    const price = document.getElementById(id).innerText;
+    const convertPrice = parseInt(price);
+    return convertPrice;
+  }
+  
 
 
 
@@ -77,6 +109,7 @@ function hideElement(elmentid){
 
 function addElement(elmentid){
   const add = document.getElementsByClassName(elmentid);
+  return add;
   
 }
 
